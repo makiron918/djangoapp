232fdc0571bdcb902f53cb79cf76d07c49a449ec
makiron918 <gonzaress918@gmail.com>
2021-08-01 10:25:21 +0900

    add templates

--- myblogapp/posts/views.py
+++ myblogapp/posts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello World! このページは投稿のインデックスです。")
+  #return HttpResponse("Hello World! このページは投稿のインデックスです。")
+  return render(request, 'posts/index.html')
 
 # Create your views here.
 
