eef7dd572be03790fc5aa26c7604ae585ea67220
makiron918 <gonzaress918@gmail.com>
2021-08-09 09:39:00 +0900

    Update urls.py

--- myblogapp/myblogapp/urls.py
+++ myblogapp/myblogapp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
+from posts import views
 
 urlpatterns = [
     url(r'^posts/', include('posts.urls')),
